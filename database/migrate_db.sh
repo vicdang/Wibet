8d5824f2e4a9f6e81f6e90c29beb1797398cfa90
Vic Dang <vudnn.dl@gmail.com>
2022-11-05 23:03:16 +0700

    remote obsoleted back up sql files

--- database/migrate_db.sh
+++ database/migrate_db.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-time=`date +%s`; mysqldump --defaults-extra-file=db.conf sgtn_18244028_wc2022 > sgtn_${time}_wc2022
+time=`date +%s`; mysqldump --defaults-extra-file=db.conf sgtn_18244028_wc2022 > sgtn_${time}_wc2022.sql
