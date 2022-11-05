f772aab295a47a738d63cf012fdc61984f6bdbff
Vic Dang <vudnn.dl@gmail.com>
2022-11-05 22:53:57 +0700

    change db config

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=sgtn_18244028_euro2021',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=sgtn_18244028_wc2022',
     'username' => 'root',
-    'password' => 'ubuntu',
+    'password' => 'password',
     'charset' => 'utf8',
     'on afterOpen' => function($event) {
                 $event->sender->createCommand("SET time_zone = '+7:00'")->execute();
