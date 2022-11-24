5665b1c32604647f43ed8086a727e8aa00a25ad3
Vic Dang <vudnn.dl@gmail.com>
2022-11-24 18:26:11 +0700

    disable debug

--- web/js/custom.js
+++ web/js/custom.js
@@ -57,7 +57,7 @@ $(document).ready(function () {
     
     function getMatch() { 
         $.ajax({
-            url: "https://txstories.live/api/get-match-by-date",
+            url: "https://txstories.live/api/get-match-by-date/",
             type: "GET",
             success: function(res) {
                 match_list = JSON.parse(res);
